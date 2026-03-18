1600f749af6f2c557f4e3e0a8dffe5941fca2965
rezaul3950 <mdrezaulkarim4119@gmail.com>
2026-03-19 05:43:41 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let darkModeIcon = document.querySelector('#darkMode-icon');
+
+darkModeIcon.onclick = () => {
+  darkModeIcon.classList.toggle('bx-sun');
+  document.body.classList.toggle('light-mode');
+};
